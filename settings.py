cff643ec53eb709680821debed7c47a6804ed175
Sergey Pakhtusov <pascal65536@gmail.com>
2023-02-15 20:51:16 +0700

    add variables

--- settings.py
+++ settings.py
@@ -1,1 +1,12 @@
 
+white = (255, 255, 255)
+yellow = (255, 255, 102)
+black = (0, 0, 0)
+red = (213, 50, 80)
+green = (0, 255, 0)
+blue = (50, 153, 213)
+
+dis_width = 600
+dis_height = 400
+
+caption = 'Zmey'
