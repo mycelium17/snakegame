81669a7cd405e0d6525d579c57c0a7e1c9a8b26a
mycelium17 <alisergribov@gmail.com>
2023-02-27 18:03:59 +0700

    Merge branch 'main' of C:\Users\gribo\Documents\GitHub\snakegame with conflicts.

--- main.py
+++ main.py
@@ -1,183 +1,248 @@
-import os
 import pygame
+import time
 import random
 import database
+from pygame_menu import themes
+import settings
+from time import sleep
 import pygame_menu
-import settings_snake as ss
+
+pygame.init()
+
+white = settings.white
+yellow = settings.yellow
+black = settings.black
+red = settings.red
+green = settings.green
+blue = settings.blue
+
+dis_width = settings.dis_width
+dis_height = settings.dis_height
 
 
-white = pygame.colordict.THECOLORS["whitesmoke"]
-blue = pygame.colordict.THECOLORS["deepskyblue1"]
-yellow = pygame.colordict.THECOLORS["lightyellow"]
-black = pygame.colordict.THECOLORS["black"]
-green = pygame.colordict.THECOLORS["forestgreen"]
-red = pygame.colordict.THECOLORS["tomato3"]
+dis = pygame.display.set_mode((dis_width, dis_height))
+pygame.display.set_caption(settings.caption)
+
+clock = pygame.time.Clock()
+
+snake_block = 10
+snake_speed = 10
+
+font_style = pygame.font.SysFont("ubuntu", 15)
+score_font = pygame.font.SysFont("ubuntu", 25)
 
 
-event_dct = {
-    pygame.K_LEFT: (-1, 0),
-    pygame.K_RIGHT: (1, 0),
-    pygame.K_UP: (0, -1),
-    pygame.K_DOWN: (0, 1),
-}
-
-
-def get_random(max, size):
-    ret = size * random.randrange(0, max // size)
-    return ret
-
-
-def food_coords(snake_list):
-    """
-    Подбор места для появления еды
-    """
-    while True:
-        foodx = get_random(surface.get_width(), ss.seed)
-        foody = get_random(surface.get_height(), ss.seed)
-        if [foodx, foody] not in snake_list:
-            return foodx, foody
-       
-        
-def message(line, msg, color):
-    """
-    Вывод сообщения по центру экрана, в зависимости от размера надписи
-    """
-    font_path = os.path.join(ss.folder_name, ss.font_name)
-    font_style = pygame.font.SysFont(font_path, 30)
-    mesg = font_style.render(f'{line}. {msg}', True, color)
-    surface.blit(
-        mesg,
-        [
-            (130),
-            (line * mesg.get_height() * 1.4 + 20),
-        ],
-    )
+def Timers():
+    value = score_font.render("Время: " + str(1), True, black)
+    dis.blit(value, [225, 0])
 
 
 def winners():
-    winners_dct = database.read_database()
-    game_over = False
-    while not game_over:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                game_over = True
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    game_over = True    
-                if event.key == pygame.K_SPACE:
-                    game_over = True
-                if event.key == pygame.K_RETURN:
-                    game_over = True
-        surface.fill(green)
-        
-        for k, user_dct in winners_dct.items():
-            msg = f'{text_dct["score"]} {user_dct["score"]} {text_dct["duration"]} {user_dct["duration"]} {text_dct["user"]} {user_dct["username"]}'
-            message(int(k), msg, yellow)
-        pygame.display.update()
-        
+    return read_database()
 
-def snake():
-    clock = pygame.time.Clock()
-    user_name = user_input.get_value()
-    
-    x1 = get_random(surface.get_width(), ss.seed)
-    y1 = get_random(surface.get_height(), ss.seed)
+def Your_score(score):
+    value = score_font.render("Ваши очки: " + str(score), True, black)
+    dis.blit(value, [0, 0])
+
+
+def our_snake(snake_block, snake_list):
+    for x in snake_list:
+        pygame.draw.rect(dis, black, [x[0], x[1], snake_block, snake_block])
+
+
+def message(msg, color):
+    mesg = font_style.render(msg, True, color)
+    dis.blit(mesg, [dis_width / 6, dis_height / 3])
+
+
+def gameLoop():
+    game_over = False
+    game_close = False
+
+    x1 = dis_width / 2
+    y1 = dis_height / 2
 
     x1_change = 0
     y1_change = 0
 
     snake_lst = []
-    snake_length = 1
+    length_of_snake = 1
 
-    foodx, foody = food_coords(snake_lst)
-    
-    tick_tmp = pygame.time.get_ticks()
-    caption_tmp = pygame.display.get_caption()
-    game_over = False
+    o = 'start'
+
+    foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
+    foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
+
+
+
     while not game_over:
+
+        while game_close == True:
+
+            pygame.init()
+            surface = pygame.display.set_mode((600, 400))
+
+            mainmenu = pygame_menu.Menu('Список лидеров', 600, 400, theme=themes.THEME_SOLARIZED)
+            mainmenu.add.label('Имя1')
+            mainmenu.add.label('счёт1')
+            mainmenu.add.label('Имя2')
+            mainmenu.add.label('счёт2')
+            mainmenu.add.label('Имя3')
+            mainmenu.add.label('счёт3')
+            mainmenu.add.button('Выйти', pygame_menu.events.EXIT)
+
+            arrow = pygame_menu.widgets.LeftArrowSelection(arrow_size=(10, 15))
+
+            update_loading = pygame.USEREVENT + 0
+
+            while True:
+                events = pygame.event.get()
+                for event in events:
+                    if event.type == pygame.QUIT:
+                        exit()
+
+                if mainmenu.is_enabled():
+                    mainmenu.update(events)
+                    mainmenu.draw(surface)
+                    if (mainmenu.get_current().get_selected_widget()):
+                        arrow.draw(surface, mainmenu.get_current().get_selected_widget())
+
+                pygame.display.update()
+
         for event in pygame.event.get():
+
             if event.type == pygame.QUIT:
                 game_over = True
 
             if event.type == pygame.KEYDOWN:
-                x, y = event_dct.get(event.key, (0, 0))
-                if x != 0 or y != 0:
-                    x1_change = x * ss.seed
-                    y1_change = y * ss.seed
+                if event.key == pygame.K_LEFT:
+                    if o == 'up' or o == 'down':
+                        x1_change = -snake_block
+                        y1_change = 0
+                        o = 'left'
+                    else:
+                        pass
+                elif event.key == pygame.K_RIGHT:
+                    if o == 'up' or o == 'down':
+                        x1_change = snake_block
+                        y1_change = 0
+                        o = 'right'
+                    else:
+                        pass
+                elif event.key == pygame.K_UP:
+                    if o == 'left' or o == 'right' or o == 'start':
+                        y1_change = -snake_block
+                        x1_change = 0
+                        o = 'up'
+                    else:
+                        pass
+                elif event.key == pygame.K_DOWN:
+                    if o == 'left' or o == 'right':
+                        y1_change = snake_block
+                        x1_change = 0
+                        o = 'down'
+                    else:
+                        pass
 
-        if x1 >= width or x1 < 0 or y1 >= height or y1 < 0:
-            game_over = True
+        if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
+            game_close = True
 
         x1 += x1_change
         y1 += y1_change
+        dis.fill(blue)
 
-        surface.fill(blue)
-        pygame.draw.rect(surface, red, [foodx, foody, ss.seed, ss.seed])
-        
+        pygame.draw.rect(dis, green, [foodx, foody, snake_block, snake_block])
         snake_head = []
         snake_head.append(x1)
         snake_head.append(y1)
         snake_lst.append(snake_head)
-        if len(snake_lst) > snake_length:
+        if len(snake_lst) > length_of_snake:
             del snake_lst[0]
 
         for x in snake_lst[:-1]:
             if x == snake_head:
-                game_over = True
+                game_close = True
 
-        for x in snake_lst:
-            pygame.draw.rect(surface, black, [x[0], x[1], ss.seed, ss.seed])
-            
-        if x1 == foodx and y1 == foody:
-            foodx, foody = food_coords(snake_lst)
-            snake_length += 1
-        
-        ticks60 = (pygame.time.get_ticks() - tick_tmp) // 60
-        caption = f'{text_dct["user"]} {user_name} | {text_dct["score"]} {snake_length} | {text_dct["duration"]} {ticks60}'
-        pygame.display.set_caption(caption)
+        our_snake(snake_block, snake_lst)
+        Your_score(length_of_snake - 1)
+        Timers()
+
         pygame.display.update()
-        clock.tick(ss.seed)
-    
-    database.update_database(username=user_name, score=snake_length, duration=ticks60)
-    pygame.display.set_caption(caption_tmp[0])
-    
+
+        if x1 == foodx and y1 == foody:
+            foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
+            foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
+            length_of_snake += 1
+        clock.tick(snake_speed)
+
+    pygame.quit()
+    quit()
+
+
+
+pygame.init()
+surface = pygame.display.set_mode((600, 400))
+
+
+def set_difficulty(value, difficulty):
+    print(value)
+    print(difficulty)
+
+
+def start_the_game():
+    mainmenu._open(loading)
+    pygame.time.set_timer(update_loading, 30)
+
+
+def level_menu():
+    mainmenu._open(level)
+
 
 def select_language(value, lang):
-    init_dct = database.load_json(ss.folder_name, ss.file_name)
-    init_dct['language'] = lang
-    database.save_json(ss.folder_name, ss.file_name, init_dct)
+    print(value)
+    print(lang)
 
-                        
-if __name__ == '__main__':
-    init_dct = database.load_json(ss.folder_name, ss.file_name)
-    lang = init_dct.get('language', 'en')
-    text_dct = ss.language[lang]['text']
-    name_cur = ss.language[lang]['name']
-    # lang_menu = [("English", 'en'), ("Русский", 'ru')]
-    lang_menu = list()
-    for k, v in ss.language.items():
-        menu_tpl = (v['name'], k)
-        if lang == k:
-            lang_menu.insert(0, menu_tpl)
-        else:
-            lang_menu.append(menu_tpl)
-        
-    width = ss.width
-    height = ss.height
-    
-    pygame.init()
-    surface = pygame.display.set_mode((width, height))
-    pygame.display.set_caption(text_dct['caption'])
-    menu = pygame_menu.Menu(
-        text_dct['menu'], 
-        width, height, 
-        theme=pygame_menu.themes.THEME_ORANGE
-        )
-    
-    user_input = menu.add.text_input(text_dct['user'], default=database.get_fake_name())
-    menu.add.selector(text_dct['language'], lang_menu, onchange=select_language)
-    menu.add.button(text_dct['start'], snake)
-    menu.add.button(text_dct['winners'], winners)
-    menu.add.button(text_dct['exit'], pygame_menu.events.EXIT)
 
-    menu.mainloop(surface)
+def lang_menu():
+    mainmenu._open(language)
+
+
+mainmenu = pygame_menu.Menu('Snake', 600, 400, theme=themes.THEME_SOLARIZED)
+mainmenu.add.text_input('Имя: ', default='')
+mainmenu.add.button('Играть', start_the_game)
+mainmenu.add.button('Сложность', level_menu)
+mainmenu.add.button('Язык', lang_menu)
+mainmenu.add.button('Выйти', pygame_menu.events.EXIT)
+
+level = pygame_menu.Menu('Выберите сложность', 600, 400, theme=themes.THEME_BLUE)
+level.add.selector('Сложность :', [('Тяжёлая', 1), ('Лёгкая', 2)], onchange=set_difficulty)
+
+language = pygame_menu.Menu('Выберите Язык', 600, 400, theme=themes.THEME_BLUE)
+language.add.selector('Язык :', [('English', 1), ('Русский', 2)], onchange=select_language)
+
+loading = pygame_menu.Menu('Loading the Game...', 600, 400, theme=themes.THEME_DARK)
+loading.add.progress_bar("Progress", progressbar_id="1", default=0, width=200, )
+
+arrow = pygame_menu.widgets.LeftArrowSelection(arrow_size=(10, 15))
+
+update_loading = pygame.USEREVENT + 0
+
+while True:
+    events = pygame.event.get()
+    for event in events:
+        if event.type == update_loading:
+            progress = loading.get_widget("1")
+            progress.set_value(progress.get_value() + 1)
+            if progress.get_value() == 100:
+                gameLoop()
+        if event.type == pygame.QUIT:
+            exit()
+
+    if mainmenu.is_enabled():
+        mainmenu.update(events)
+        mainmenu.draw(surface)
+        if (mainmenu.get_current().get_selected_widget()):
+            arrow.draw(surface, mainmenu.get_current().get_selected_widget())
+
+    pygame.display.update()
+
